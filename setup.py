82c20e3b09f73e0a9efeb558f8aecf0c850c2a36
n0x1s <ilyasselfatimy9@gmail.com>
2020-06-23 17:14:59 +0100

    pypi ready

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='cached_properties',
+    packages=['cached_properties'],
+    version='0.5',
+    license='MIT',
+    description='class/method decorator for caching properties',
+    author='n0x1s',
+    author_email='n0x1s0x01@gmail.com',
+    url='https://github.com/n0x1s/cached_properties',
+    download_url='https://github.com/N0x1s/cached_properties/archive/0.5.tar.gz',
+    keywords=['property cache', 'cache decorator', 'cache',
+              'decorator'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
