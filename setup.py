28383fe86024b5dbb0c684b0b9449d4d37cd708d
N0x1s <ilyasselfatimy9@gmail.com>
2020-09-03 07:34:20 -0400

    bump the version

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 setup(
     name='cached_properties',
     packages=['cached_properties'],
-    version='0.7.1',
+    version='0.7.3',
     license='MIT',
     description='class/method decorator for caching properties',
     author='n0x1s',
     author_email='n0x1s0x01@gmail.com',
     url='https://github.com/n0x1s/cached_properties',
-    download_url='https://github.com/N0x1s/cached_properties/archive/0.7.1.tar.gz',
+    download_url='https://github.com/N0x1s/cached_properties/archive/0.7.3.tar.gz',
     keywords=['property cache', 'cache decorator', 'cache',
-              'decorator'],
+              'decorator', 'cached properties'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
