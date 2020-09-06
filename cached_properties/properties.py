1afd0e92793a82a22eda1e192b647e2c48a41533
N0x1s <ilyasselfatimy9@gmail.com>
2020-09-06 18:11:09 -0400

    renamed Property to cproperty, and added classdecorator class

--- cached_properties/properties.py
+++ cached_properties/properties.py
@@ -1,9 +1,10 @@
+import inspect
 from time import time
 
-__all__ = ['Property']
+__all__ = ['cproperty', 'classproperty', 'Property']
 
 
-class Property:
+class cproperty:
     def __init__(self, method=None, *, general=None, timeout=None, hits=None,
                  validator=None):
         self.general = general
@@ -77,3 +78,31 @@ class Property:
                     'timeout': (time(), self.timeout),
                     'hits': self.hits})
         return self.storage[self._var]['value']
+
+
+class classdecorator:
+    def __init__(self, cls=None, methods=None, auto=False, **kwargs):
+        self.cls = cls
+        self.methods = methods
+        self.auto = auto
+        self.kwargs = kwargs
+
+    def __call__(self, cls=None, *args, **kwargs):
+        icls = self.cls if self.cls else cls
+        if self.auto or not self.methods:
+            for name, value in vars(icls).items():
+                if callable(value) and len(
+                        list(inspect.signature(value).parameters)) == 1:
+                    setattr(icls, name, Property(value, **self.kwargs))
+        else:
+            for method in self.methods:
+                setattr(icls, method, Property(getattr(cls, method),
+                                               **self.kwargs))
+        return icls(cls, *args, **kwargs) if self.cls else icls
+
+
+class Property(cproperty):
+    def __init__(self, *args, **kwargs):
+        print('Property will be removed in leter versions \
+please take a look at cproperty instead many thanks')
+        super().__init__(*args, **kwargs)
