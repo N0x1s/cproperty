bc706605c66db1415e4f29fe729f586f9a5eabe2
n0x1s <ilyasselfatimy9@gmail.com>
2021-01-26 14:42:02 +0100

    __init__ signature suppoer default values

--- cproperty/__init__.py
+++ cproperty/__init__.py
@@ -3,7 +3,8 @@
 import asyncio
 import threading
 from functools import wraps
-
+from itertools import zip_longest
+from inspect import Signature, Parameter
 # from multiprocessing import Process
 
 
@@ -19,15 +20,29 @@ def thread_safe(func):
     def wrapped(self, *args, **kwargs):
         if not getattr(self, 'lock'):
             self.lock = threading.Lock()
-
         with self.lock:
             return func(self, *args, **kwargs)
     return wrapped
 
 
 class cproperty:
-    def __init__(self, /, method=None, *, hits=None):
-        self.hits = hits
+    _fields = ('general', 'timeout', 'hits', 'validator')
+    _sig = Signature([Parameter(name, Parameter.POSITIONAL_OR_KEYWORD)
+                                    for name in _fields])
+
+    def __init__(self, method=None, /, *args, **kwargs):
+
+        for name, value in zip_longest(self._fields, self._sig.bind_partial(
+                            *args, **kwargs).arguments.values()):
+            setattr(self, name, value)
+
+        for name, value in self._sig.bind_partial(*args, **kwargs).arguments.items():
+            setattr(self, name, value)
+
+        for name in self._fields:
+            if not hasattr(self, name):
+                setattr(self, name, None)
+
         self.lock = threading.Lock()
         if method:
             self._setup_method(method)
@@ -38,27 +53,34 @@ class cproperty:
 
     def _setup_method(self, method):
         self.method = method
+        self.cache_key = f'{method.__name__}_cache'
+
 
     def _initialed_storage(self, instance):
         if not hasattr(self, 'storage'):
             self.storage = instance.__dict__
-            self.storage['value'] = self._compute_value(instance)
+        if self.cache_key not in self.storage:
+            self.storage[self.cache_key] = {name: value for name, value in self.__dict__.items()
+                            if name in self._fields}
+            self.storage[self.cache_key]['value'] = self._compute_value(instance)
+
 
     def _compute_value(self, instance):
         return self.method(instance)
 
+
     @thread_safe
     @check_storage
     def __get__(self, instance, owner):
-        return self.storage['value']
+        return self.storage[self.cache_key]['value']
+        
 
     @thread_safe
     @check_storage
     def __set__(self, instance, value):
-
-        self.storage['value'] = value
+        self.storage[self.cache_key]['value'] = value
 
     @thread_safe
     @check_storage
     def __delete__(self, instance):
-        del self.storage
+        del self.storage[self.cache_key]
